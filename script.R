b320316334e768a58ff8ecebfa03d04cb23084e4
Cesar G <cesar.mgv@outlook.com>
2022-10-12 14:40:49 -0500

    primer commit

---
+++ script.R
@@ -0,0 +1,14 @@
+install.packages("httr")
+install.packages("jsonlite")
+library(rvest)
+library(httr)
+library(jsonlite)
+
+# guardamos la url en una variable
+
+url <- "https://opensky-network.org/api/states/all"
+datos <- GET(url)
+datos
+
+datos <- fromJSON(content(datos, type = "text"))
+datos <- datos[["states"]]
