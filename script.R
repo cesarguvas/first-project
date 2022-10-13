80f34edc3cbcb0e37474aa2c41acf15fa7969ba3
Cesar <cesar.gv16@gmail.com>
2022-10-12 23:44:05 -0500

    nada

--- script.R
+++ script.R
@@ -13,6 +13,24 @@ datos
 datos <- fromJSON(content(datos, type = "text"))
 datos <- datos[["states"]]
 
-# scrapeando de la web
+# extraemos las tablas de la pagina donde se encuentra la informacion
+
+url <- read_html("https://openskynetwork.github.io/opensky-api/rest.html")
+
+tablas <- url %>% html_table()
+
+# en la tabla 5 esta la propiedad de los estados de nuestro dataframe 'datos'
+
+tablas[[5]]
+
+# eliminamos el ultimo registro ya que tiene 18, y el dataframe tiene 17
+
+coltablas <- tablas[[5]]
+
+coltablas <- coltablas[-18, ]
+
+# asignamos los nombres al dataframe datos
+
+colnames(datos) <- coltablas$Property
 
 
