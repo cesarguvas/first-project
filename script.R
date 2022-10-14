c8a5c5946c4f3168f496a19c78dfcb3dfb1d3d00
Cesar <cesar.gv16@gmail.com>
2022-10-14 07:50:52 -0500

    mejorando la explicacion

--- script.R
+++ script.R
@@ -4,19 +4,29 @@ library(rvest)
 library(httr)
 library(jsonlite)
 
-# guardamos la consulta a la api en 'url' y luego extaemos con GET 
+# guardamos la consulta api en 'url, luego extraemos la informacion de la
+# consulta con GET y lo asignamos a 'datos'
 
 url <- "https://opensky-network.org/api/states/all"
 datos <- GET(url)
 datos
 
-
-# extraemos el contenido
+# la respuesta es un formato json, utilizo la funcion content para extraer como 
+# texto ya que la respuesta es una respuesta de servidor, y la funcion fromJSON 
+# para convertirlo en un objeto de R 
 
 datos <- fromJSON(content(datos, type = "text"))
+
+# ahora tenemos una lista de 2 elementos, la que nos interesa es 'states'
+
 datos <- datos[["states"]]
 
-# extraemos las tablas de la pagina donde se encuentra la informacion
+# ahora tenemos una matriz que es lo que queremos, solo nos falta asignar los
+# nombres a nuestras columnas.Los nombres correspondientes a cada columna estan 
+# en la pagina de documentacion de la api, podemos asignarlo manualmente.
+
+# O podemos extraer las tablas de la pagina donde se encuentra la informacion y 
+# asignarlo haciendo algo de web scraping basico:
 
 url2 <- read_html("https://openskynetwork.github.io/opensky-api/rest.html")
 
